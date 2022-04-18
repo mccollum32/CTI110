cc95354f5c4e620dc107c4452fedd83e76f69860
mccollum32 <103016879+mccollum32@users.noreply.github.com>
2022-04-18 17:20:57 -0400

    Remove Gray

---
+++ P3LAB1_McCollum_Joseph.py
@@ -0,0 +1,17 @@
+red = int(input())
+green = int(input())
+blue = int(input())
+gray = 50
+
+if (red <= green) and ( red <= blue):
+    gray = red 
+if (green <= red) and ( green <= blue):
+    gray = green
+if (blue <= green) and ( blue <= red):
+    gray = blue
+
+red = red - gray
+green = green - gray
+blue = blue - gray
+
+print(red, green , blue)
