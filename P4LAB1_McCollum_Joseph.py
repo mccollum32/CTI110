ff0d07b4a5abdcc2aa2860660009170221971cd5
mccollum32 <103016879+mccollum32@users.noreply.github.com>
2022-05-03 23:26:38 -0400

    P4LAB1-Count Input Length
    P4LAB1-Count Input Length

---
+++ P4LAB1_McCollum_Joseph.py
@@ -0,0 +1,10 @@
+def main():
+    user_text = input()
+    numOfChars = 0 
+    count = 0 
+    for i in user_text:
+        if user_text[count] != ' ' and user_text[count] != '.' and user_text[count] != ',':
+            numOfChars += 1
+        count += 1
+    print(numOfChars)
+main()
